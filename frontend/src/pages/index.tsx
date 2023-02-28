105774cd4b3720ac80c036655ae9ca8ba8555e5b
NoahCardoza <noahcardoza@gmail.com>
2023-02-27 17:00:54 -0800

    APP-37: install husky

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -17,7 +17,6 @@ function IndexPage() {
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
