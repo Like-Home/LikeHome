46f80dd42b2d581921b02ebcd063e816b6745202
NoahCardoza <noahcardoza@gmail.com>
2023-02-27 16:39:13 -0800

    APP-37: setup linting, formatting, and commit hooks

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -14,18 +14,17 @@ function IndexPage() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://reactjs.org" target="_blank">
+
+        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -39,7 +38,7 @@ function IndexPage() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
 export default IndexPage
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'static'
-})
+  plugins: [
+    eslint({
+      fix: true,
+    }),
+    react(),
+  ],
+  publicDir: 'static',
+});
