c89c053e26e935a69f64dadb2bbd0ff1c4cf556d
NoahCardoza <noahcardoza@gmail.com>
2023-02-27 19:31:40 -0800

    APP-14: configure google oauth automatically from env

---
+++ backend/app/config.py
@@ -0,0 +1,45 @@
+"""Gives auto complete to all env variables and wraps other config-like files."""
+
+import os
+from typing import Any
+
+
+class Sentinel:
+    """Stores placeholder values for function arguments. 
+    Similar to Symbol in JavaScript.
+    """
+
+    def __init__(self, name=''):
+        self.name = f"Sentinel({name})"
+
+    def __repr__(self):
+        return self.name
+
+
+NotSet = Sentinel('NOT_SET')
+
+
+def getenv(key: str, default: Any = NotSet) -> Any:
+    """Retrieves a value from the environment.
+
+    Args:
+        key (str): The variable name.
+        default (Any, optional): A default value to fall back on. Defaults to NotSet.
+
+    Raises:
+        EnvironmentError: When no default has been set and the variable is not found.
+
+    Returns:
+        Any: The value of the ENV variable unless missing and default was set.
+    """
+
+    if key in os.environ:
+        return os.environ[key]
+
+    if default is NotSet:
+        raise EnvironmentError(
+            f'The environment variable "{key}" was not found and no default was set.')
+
+
+GOOGLE_CLIENT_ID = getenv('GOOGLE_CLIENT_ID')
+GOOGLE_CLIENT_SECRET = getenv('GOOGLE_CLIENT_SECRET')
--- backend/app/settings.py
+++ backend/app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from . import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -136,6 +138,11 @@ ACCOUNT_EMAIL_REQUIRED = True
 
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
+        'APP': {
+            'client_id': config.GOOGLE_CLIENT_ID,
+            'secret': config.GOOGLE_CLIENT_SECRET,
+            'key': ''
+        },
         'SCOPE': [
             'profile',
             'email',
