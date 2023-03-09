cfd32c93594620edccee5b156ed44f2a75cb6ce7
Josh <joshua.medina@sjsu.edu>
2023-03-09 13:17:55 -0800

    APP-58: Fixed duplicated file in pages

--- frontend/src/pages/booking.tsx
+++ frontend/src/pages/booking.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 function Booking() {
-  const { id } = useParams();
+  const { bookingid } = useParams();
 
   return (
     <div>
       <h1>Booking</h1>
-      <pre>bookingId: {id}</pre>
+      <pre>bookingId: {bookingid} </pre>
     </div>
   );
 }
