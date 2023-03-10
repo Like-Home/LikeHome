d6a8c37c266b2b581125afe4ff23537d8c123ec5
Razzacktiger <harrazzack@gmail.com>
2023-03-09 16:55:17 -0800

    APP-23 - first amadeus api config

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -184,3 +184,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from amadeus import Client, ResponseError
+amadeus = Client(
+    client_id=AMADEUS_CLIENT_ID,
+    client_secret=AMADEUS_CLIENT_SECRET,
+    log_level='debug'
+)
