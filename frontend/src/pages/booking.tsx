faa08c64a118eecf94f071592f38f32ad5952729
Alpyne <alpyne.dreams@gmail.com>
2023-03-09 18:20:35 -0800

    APP-58: Consistent names for Pages

--- frontend/src/pages/booking.tsx
+++ frontend/src/pages/booking.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-function Booking() {
+function BookingPage() {
   const { bookingid } = useParams();
 
   return (
@@ -12,4 +12,4 @@ function Booking() {
   );
 }
 
-export default Booking;
+export default BookingPage;
