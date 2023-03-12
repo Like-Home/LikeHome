5e6b64c326824c670aa8a5126c5341574bb39237
RishiJ12 <rishijagtap12@gmail.com>
2023-03-11 22:18:50 -0800

    APP-33: Added hotel search

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,5 @@
+import traceback
+
 from amadeus import Location, ResponseError
 from api.modules.amadeus import amadeus
 from django.contrib.auth.models import User
@@ -16,15 +18,40 @@ from .serializers import BookingSerializer, UserSerializer
 def search_city(req, param):
     if req.method == "GET":
         try:
-            print(param)
-            response = amadeus.reference_data.locations.get(
-                keyword=param, subType=Location.ANY)
-            context = {
-                "data": response.data
-            }
-            return JsonResponse(context)
+            city_info = amadeus.reference_data.locations.get(
+                keyword=param, subType=Location.CITY)
+            city_code = []
+            for i in city_info.data:
+                city_code.append(i['iataCode'])
+            return JsonResponse({
+                "data": city_code
+            })
         except ResponseError as error:
             print(error)
+        return JsonResponse({"error": "Invalid request"})
+
+
+def search_hotel(req, citycode, checkindata, checkoutdata, rooms, travelers):
+    if req.method == "GET":
+        try:
+            print(citycode)
+            hotel_list = amadeus.reference_data.locations.hotels.by_city.get(
+                cityCode=citycode)
+
+            hotel_names = []
+            for i in hotel_list.data:
+                hotel_names.append(i['hotelId'])
+            print(hotel_names)
+            hotel_offers = amadeus.shopping.hotel_offers_search.get(
+                hotelIds=hotel_names[:10], adults=travelers, checkInDate=checkindata,
+                checkOutDate=checkoutdata,
+                roomQuantity=rooms)
+            print(hotel_offers.data)
+            return JsonResponse({"data": hotel_offers.data})
+        except ResponseError as error:
+            traceback.print_exc()
+            print(error.response.data)
+            print(error.code)
         return JsonResponse({"error": "Invalid request"})
 
 
