863595a8976d51fdfc226e100600faeb2105cdb5
NoahCardoza <noahcardoza@gmail.com>
2023-03-14 11:17:39 -0700

    APP-25: move router to new file and guard protected routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,80 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RouterProvider } from 'react-router-dom';
 
 import { RecoilRoot } from 'recoil';
-import HomePage from './pages/home';
-import HotelPage from './pages/hotel';
-import AuthPage from './pages/auth';
-import BookingPage from './pages/booking';
-import SearchPage from './pages/search';
-import CheckoutPage from './pages/checkout';
-import BookingsPage from './pages/bookings';
-import AboutPage from './pages/about';
-import RewardsPage from './pages/rewards';
+import router from './router';
 
 import './index.scss';
 
-import ErrorLayout from './layouts/error';
-import RootLayout from './layouts/root';
 import { CssBaseline } from '@mui/material';
+
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-// ensures a CSRF token is set in the cookies
-fetch('/api/csrf');
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorLayout />,
-    children: [
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path: '/auth',
-        element: <AuthPage />,
-      },
-      {
-        path: '/hotel/:hotelId',
-        element: <HotelPage />,
-      },
-      {
-        path: '/booking/:id',
-        element: <BookingPage />,
-      },
-      {
-        path: '/search/:query',
-        element: <SearchPage />,
-      },
-      {
-        path: '/checkout',
-        element: <CheckoutPage />,
-      },
-      {
-        path: '/bookings',
-        element: <BookingsPage />,
-      },
-      {
-        path: '/about',
-        element: <AboutPage />,
-      },
-      {
-        path: '/rewards',
-        element: <RewardsPage />,
-      },
-    ],
-  },
-]);
 
 const darkTheme = createTheme({
   palette: {
     mode: 'dark',
   },
 });
+
+// ensures a CSRF token is set in the cookies
+fetch('/api/csrf');
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
---
+++ frontend/src/router.tsx
@@ -0,0 +1,76 @@
+import { createBrowserRouter, Outlet, Navigate } from 'react-router-dom';
+
+import HomePage from './pages/home';
+import HotelPage from './pages/hotel';
+import AuthPage from './pages/auth';
+import BookingPage from './pages/booking';
+import SearchPage from './pages/search';
+import CheckoutPage from './pages/checkout';
+import BookingsPage from './pages/bookings';
+import AboutPage from './pages/about';
+import RewardsPage from './pages/rewards';
+
+import ErrorLayout from './layouts/error';
+import RootLayout from './layouts/root';
+
+import userAtom from './recoil/user';
+import { useRecoilValue } from 'recoil';
+
+const AuthGuard = () => {
+  const user = useRecoilValue(userAtom);
+  // TODO: notify user that they need to login
+  return user === null ? <Navigate to="/auth" /> : <Outlet />;
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorLayout />,
+    children: [
+      {
+        path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/auth',
+        element: <AuthPage />,
+      },
+      {
+        path: '/hotel/:hotelId',
+        element: <HotelPage />,
+      },
+      {
+        path: '/search/:query',
+        element: <SearchPage />,
+      },
+      {
+        path: '/about',
+        element: <AboutPage />,
+      },
+      {
+        element: <AuthGuard />,
+        children: [
+          {
+            path: '/booking/:id',
+            element: <BookingPage />,
+          },
+          {
+            path: '/bookings',
+            element: <BookingsPage />,
+          },
+          {
+            path: '/rewards',
+            element: <RewardsPage />,
+          },
+          {
+            path: '/checkout',
+            element: <CheckoutPage />,
+          },
+        ],
+      },
+    ],
+  },
+]);
+
+export default router;
