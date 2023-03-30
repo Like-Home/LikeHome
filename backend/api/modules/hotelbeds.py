07aef8bcf680015876eb4ce55d137cf03222c8b6
NoahCardoza <noahcardoza@gmail.com>
2023-03-29 17:19:46 -0700

    APP-29: setup caching for hotelbeds api in dev mode

--- backend/api/modules/hotelbeds.py
+++ backend/api/modules/hotelbeds.py
@@ -1,9 +1,17 @@
 import hashlib
+import json
+import pickle
 import time
+from collections import OrderedDict
+from hashlib import md5
+from logging import getLogger
+from operator import itemgetter
 
+import diskcache as dc
+from app import config
 from requests import Session
 
-from app import config
+logger = getLogger('hotelbeds')
 
 
 def generate_signature(api_key: str, secret: str) -> str:
@@ -22,14 +30,29 @@ def generate_signature(api_key: str, secret: str) -> str:
     ).hexdigest()
 
 
+head = itemgetter(0)
+
+
+def convert_to_nested_ordered_dict(d):
+    return OrderedDict(
+        sorted(
+            ((k, convert_to_nested_ordered_dict(v))
+             if isinstance(v, dict) else (k, v)
+             for k, v in d.items()),
+            key=head
+        )
+    )
+
+
 class HotelbedsClient(Session):
     """A client for the Hotelbeds API."""
 
-    def __init__(self, endpoint: str, api_key: str, secret: str):
+    def __init__(self, endpoint: str, api_key: str, secret: str, cache=None):
         super().__init__()
         self.endpoint = endpoint
         self.api_key = api_key
         self.secret = secret
+        self.cache = cache
 
     def request(self, method, url, **kwargs):
         kwargs.setdefault('headers', {})
@@ -42,13 +65,35 @@ class HotelbedsClient(Session):
         if not url.startswith('http'):
             url = self.endpoint + url
 
-        return super().request(method, url, **kwargs)
+        cacheable = json.dumps({
+            'url': url,
+            'method': method,
+            'json': (kwargs.get('json') or {}),
+            'data': (kwargs.get('data') or {}),
+            'params': (kwargs.get('params') or {}),
+        }, indent=2, sort_keys=True)
+
+        key = md5(cacheable.encode('utf-8')).hexdigest()
+
+        if self.cache is not None:
+            logger.info(f'Using cache for {url}')
+            if key in self.cache:
+                return pickle.loads(self.cache[key])
+
+        res = super().request(method, url, **kwargs)
+
+        if self.cache is not None and res.ok:
+            logger.info(f'Adding to cache {url}')
+            self.cache[key] = pickle.dumps(res)
+
+        return res
 
 
 hotelbeds = HotelbedsClient(
     config.HOTELBEDS_ENDPOINT,
     config.HOTELBEDS_API_KEY,
-    config.HOTELBEDS_API_SECRET
+    config.HOTELBEDS_API_SECRET,
+    cache=dc.Cache('cache') if config.MONEY_SAVER_MODE else None
 )
 
 if __name__ == '__main__':
