c616b2f02d6632a3751fd08f81795ea16e5066ab
Alpyne <alpyne.dreams@gmail.com>
2023-04-17 00:29:45 -0700

    APP-5: Implement basic clientside filtering

--- backend/api/views/destination.py
+++ backend/api/views/destination.py
@@ -33,7 +33,7 @@ def convert_category_to_rating_props(category_description):
 class CustomPagination(pagination.PageNumberPagination):
     page_size = 5
     page_size_query_param = 'size'
-    max_page_size = 10
+    max_page_size = 100
 
     def get_paginated_response(self, data):
         return Response({
--- frontend/src/pages/search.tsx
+++ frontend/src/pages/search.tsx
@@ -1,31 +1,50 @@
-import { Stack, Typography, Button } from '@mui/material';
-
-import { useSearchParams, Link } from 'react-router-dom';
-import { useEffect, useState, Suspense } from 'react';
+import {
+  Stack,
+  Typography,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  IconButton,
+  Select,
+  MenuItem,
+  InputLabel,
+  FormControl,
+} from '@mui/material';
+import { useSearchParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import slugify from 'slugify';
-import usePromise from 'react-promise-suspense';
-import AppLoadingSpinner from '../components/AppLoadingSpinner';
+import CloseIcon from '@mui/icons-material/Close';
 import HotelCard, { onBookNowCallback } from '../components/HotelCard';
 import SearchBars, { SearchPageParams, onSearchProps } from '../components/SearchBars';
-import { getOffersByLocation } from '../api/search';
-import Result from '../components/Result';
+import { getOffersByLocation, OfferHotel } from '../api/search';
+import Spinner from '../components/Spinner';
+import PriceSlider from '../components/controls/PriceSlider';
 
-function OfferHotels({
-  kwargs,
-}: {
-  kwargs: {
-    destinationCode: string;
-    checkin: string;
-    checkout: string;
-    guests: number;
-    rooms: number;
-  };
-}) {
+type ZoneInfo = {
+  code: number | string;
+  name: string;
+  count: number;
+};
+
+export default function SearchPage() {
   const [rawParams] = useSearchParams();
   const params: SearchPageParams = Object.fromEntries([...rawParams]);
 
-  const resource = usePromise(getOffersByLocation, [kwargs]);
+  const location = JSON.parse(atob(params.location || ''));
 
+  // Data
+  const [results, setResults] = useState<OfferHotel[]>([]);
+  const [sorted, setSorted] = useState<OfferHotel[]>([]);
+  const [filtered, setFiltered] = useState<OfferHotel[]>([]);
+  const [zones, setZones] = useState<ZoneInfo[]>([]);
+  const [resultsMessage, setResultsMessage] = useState('');
+
+  // Sort and filter parameters
+  const [priceRange, setPriceRange] = useState([0, 500]);
+  const [zone, setZone] = useState<string | null>(null);
+  const [sort, setSort] = useState('suggested');
+
+  // TODO: This could be modal? Maybe
   const onBookNow: onBookNowCallback = (hotel) => {
     window.open(
       `/hotel/${hotel.code}/${slugify(hotel.name).toLowerCase()}/?checkin=${params.checkin}&checkout=${
@@ -35,81 +54,33 @@ function OfferHotels({
     );
   };
 
-  return (
-    <>
-      <Typography color="#999">Found {resource.results.length} hotels</Typography>
-      <Stack spacing={3} mt={2} mb={3}>
-        {resource.results.length === 0 ? (
-          <Result variant="info" hidePrimaryButton={true}>
-            <Stack alignItems="center" spacing={1}>
-              <Typography variant="body1" component="p">
-                There are no hotels available for the selected dates at this location.
-              </Typography>
-              <Typography variant="body1" component="p">
-                Please try again with different dates or another location.
-              </Typography>
-            </Stack>
-          </Result>
-        ) : (
-          resource.results.map((hotel) => (
-            <HotelCard key={hotel.code} hotel={hotel} onBookNow={() => onBookNow(hotel)} />
-          ))
-        )}
-      </Stack>
-    </>
-  );
-}
-
-interface setOfferHotelsArgsProps {
-  destinationCode?: string;
-  checkin?: string;
-  checkout?: string;
-  guests?: number;
-  rooms?: number;
-}
-
-function pageParamsAreInvalid(params: setOfferHotelsArgsProps) {
-  return !params.destinationCode || !params.checkin || !params.checkout || !params.guests || !params.rooms;
-}
-
-export default function SearchPage() {
-  const [rawParams] = useSearchParams();
-  const params: SearchPageParams = Object.fromEntries([...rawParams]);
-  const location = JSON.parse(atob(params.location || ''));
-
-  const [offerHotelsArgs, setOfferHotelsArgs] = useState<setOfferHotelsArgsProps>({
-    destinationCode: location?.code,
-    checkin: params.checkin,
-    checkout: params.checkout,
-    guests: Number(params.guests),
-    rooms: Number(params.rooms),
-  });
-
-  // show loading animation
   async function onSearch(kwargs: onSearchProps) {
-    // update url params
-    const searchParams = new URLSearchParams();
-    searchParams.set('location', btoa(JSON.stringify(kwargs.location)));
-    searchParams.set('checkin', kwargs.checkin || '');
-    searchParams.set('checkout', kwargs.checkout || '');
-    searchParams.set('guests', kwargs.guests || '');
-    searchParams.set('rooms', kwargs.rooms || '');
-    window.history.replaceState({}, '', `${window.location.pathname}?${searchParams.toString()}`);
+    // console.log(kwargs?.location);
 
-    setOfferHotelsArgs({
-      destinationCode: kwargs.location?.code,
+    const response = await getOffersByLocation({
+      destinationCode: kwargs?.location?.code,
       checkin: kwargs.checkin,
       checkout: kwargs.checkout,
       guests: Number(kwargs.guests),
       rooms: Number(kwargs.rooms),
+      size: 100,
     });
+
+    setResults(response.results);
+
+    // Compute set of all zones
+    const zoneSet = new Map();
+    // TODO: No zoneCode?
+    response.results.forEach((h) => {
+      if (!zoneSet.has(h.zoneName)) zoneSet.set(h.zoneName, 1);
+      else zoneSet.set(h.zoneName, zoneSet.get(h.zoneName) + 1);
+    });
+    setZones(Array.from(zoneSet).map(([z, count]): ZoneInfo => ({ code: z, name: z, count })));
   }
 
+  // Begin search
   useEffect(() => {
-    if (pageParamsAreInvalid(offerHotelsArgs)) {
-      return;
-    }
-
+    setResultsMessage('Searching...');
     onSearch({
       location,
       checkin: params.checkin,
@@ -119,16 +90,45 @@ export default function SearchPage() {
     });
   }, []);
 
-  if (pageParamsAreInvalid(offerHotelsArgs)) {
-    return (
-      <main className="card push-center" style={{ marginTop: 50, maxWidth: 1200 }}>
-        <p>Something seems to be missing in the URL.</p>
-        <Button component={Link} to="/">
-          Go Home
-        </Button>
-      </main>
+  // Apply sorting
+  useEffect(() => {
+    if (!results.length) return;
+    switch (sort) {
+      case 'suggested':
+        setSorted(results);
+        break;
+      case 'price-low':
+        setSorted([...results].sort((a, b) => Number(a.minRate) - Number(b.minRate)));
+        break;
+      case 'price-high':
+        setSorted([...results].sort((a, b) => Number(b.minRate) - Number(a.minRate)));
+        break;
+      default:
+        break;
+    }
+  }, [results, sort]);
+
+  // Apply filters
+  useEffect(() => {
+    if (!sorted.length) return;
+    let filteredList = sorted;
+
+    // Filter by price
+    filteredList = filteredList.filter(
+      (hotel) =>
+        Number(hotel.minRate) >= priceRange[0] && (!(priceRange[1] < 500) || Number(hotel.minRate) <= priceRange[1]),
     );
-  }
+
+    // Filter by zone
+    if (zone) filteredList = filteredList.filter((hotel) => hotel.zoneName === zone);
+
+    setFiltered(filteredList);
+  }, [sorted, priceRange, zone]);
+
+  useEffect(() => {
+    if (!results.length) return;
+    setResultsMessage(`Found ${filtered.length} hotel${filtered.length !== 1 ? 's' : ''}`);
+  }, [filtered]);
 
   return (
     <main className="card push-center" style={{ marginTop: 50, maxWidth: 1200 }}>
@@ -140,20 +140,68 @@ export default function SearchPage() {
         checkout={params.checkout}
         onSearch={onSearch}
       />
-      <Suspense fallback={<AppLoadingSpinner />}>
-        <OfferHotels
-          kwargs={
-            // TODO: clean up the types validated in the if above
-            offerHotelsArgs as {
-              destinationCode: string;
-              checkin: string;
-              checkout: string;
-              guests: number;
-              rooms: number;
-            }
-          }
-        />
-      </Suspense>
+      <Stack direction="row" spacing={4} sx={{ pt: 4 }}>
+        <Stack sx={{ width: 275, p: 2, pl: 1 }} alignItems="start">
+          {results.length > 0 && (
+            <>
+              <PriceSlider priceRange={priceRange} setPriceRange={setPriceRange} />
+              {zones.length > 0 && (
+                <>
+                  <Stack direction="row" justifyContent="space-between" alignItems="end" width="100%" height={40}>
+                    <Typography gutterBottom>Neighborhood</Typography>
+                    {zone && (
+                      <IconButton onClick={() => setZone(null)}>
+                        <CloseIcon />
+                      </IconButton>
+                    )}
+                  </Stack>
+                  <RadioGroup name="zones" value={zone} onChange={(e, v) => setZone(v)}>
+                    {zones.map(({ code, name, count }) => (
+                      <FormControlLabel
+                        value={code}
+                        key={code}
+                        label={
+                          <Typography style={{ fontSize: 14 }}>
+                            {name} <small>({count})</small>
+                          </Typography>
+                        }
+                        control={<Radio />}
+                      />
+                    ))}
+                  </RadioGroup>
+                </>
+              )}
+            </>
+          )}
+        </Stack>
+        <Stack sx={{ maxWidth: 600, mx: 'auto' }}>
+          <Stack direction="row" justifyContent="space-between" alignItems="end" width="100%">
+            <Typography sx={{ flex: 2 }} color="#999">
+              {resultsMessage}
+            </Typography>
+            {results.length ? (
+              <FormControl sx={{ flex: 1 }} size="small">
+                <InputLabel id="sort-by-label">Sort by</InputLabel>
+                <Select
+                  value={sort}
+                  onChange={(e) => setSort(e.target.value as string)}
+                  label="Sort by"
+                  labelId="sort-by-label"
+                >
+                  <MenuItem value="suggested">Suggested</MenuItem>
+                  <MenuItem value="price-low">Price: low to high</MenuItem>
+                  <MenuItem value="price-high">Price: high to low</MenuItem>
+                </Select>
+              </FormControl>
+            ) : null}
+          </Stack>
+          <Stack spacing={3} mt={2} mb={3}>
+            {filtered.length
+              ? filtered.map((hotel) => <HotelCard key={hotel.code} hotel={hotel} onBookNow={onBookNow} />)
+              : results.length === 0 && <Spinner />}
+          </Stack>
+        </Stack>
+      </Stack>
     </main>
   );
 }
