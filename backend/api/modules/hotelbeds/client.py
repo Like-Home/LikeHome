3e915af47028ba6f2770cecfa966ad7fc189976f
NoahCardoza <noahcardoza@gmail.com>
2023-04-23 11:02:51 -0700

    allow multiple api keys in prod

--- backend/api/modules/hotelbeds/client.py
+++ backend/api/modules/hotelbeds/client.py
@@ -7,11 +7,80 @@ from collections import OrderedDict
 from hashlib import md5
 from logging import getLogger
 from operator import itemgetter
+from typing import List, Optional, Tuple
 
 from requests import Session
 from requests.adapters import HTTPAdapter, Retry
 
 logger = getLogger('hotelbeds')
+
+
+class KeyPoolOverflow(Exception):
+    pass
+
+
+class KeyPool:
+    @classmethod
+    def from_dict(cls, data: List[dict]):
+        return cls([(pair['key'], pair['secret']) for pair in data])
+
+    @classmethod
+    def from_file(cls, file):
+        data = json.load(file)
+        return cls.from_dict(data)
+
+    def __init__(self, key_pool: List[Tuple[str, str]]):
+        """
+        Args:
+            key_pool (Tuple[str, str]): array of tuples (key, secret)
+        """
+        self.key_pool = key_pool
+        self.cur_key = 0
+
+    def get_key(self):
+        return self.key_pool[self.cur_key][0]
+
+    def get_secret(self):
+        return self.key_pool[self.cur_key][1]
+
+    def next_key(self):
+        self.cur_key += 1
+        return self.cur_key < len(self.key_pool)
+
+    def reset(self):
+        self.cur_key = 0
+
+# [{
+#     "name": "Nha",
+#     "key": "ac00df36f6a3bffadb3ba384582aaf9e",
+#     "secret": "1c211bc731"
+# },
+#     {
+#     "name": "Josh",
+#     "key": "d41651a67b68b36d5f2dbfd28145f87d",
+#     "secret": "e6062af3bf"
+# },
+#     {
+#     "name": "Jeff Throwaway",
+#     "key": "37f60277ee67df99d23144b542260e78",
+#     "secret": "eda7e0129a"
+# },
+#     {
+#     "name": "Micheal",
+#     "key": "fbd965f9ca40a9d3a9b9152d5da4fcfe",
+#     "secret": "a31b78b04e"
+# },
+#     {
+#     "name": "Rishi",
+#     "key": "ad2698d1ff021fb1c81a93f1d7d44d7b",
+#     "secret": "3e598c20c3"
+# },
+#     {
+#     "name": "Noah",
+#     "key": "319ad69c0ecef4c79861bb616a3c30ea",
+#     "secret": "a8f2bfb4b9"
+# }
+# ]
 
 
 def generate_signature(api_key: str, secret: str) -> str:
@@ -47,11 +116,22 @@ def convert_to_nested_ordered_dict(d):
 class HotelbedsClient(Session):
     """A client for the Hotelbeds API."""
 
-    def __init__(self, endpoint: str, api_key: str, secret: str, cache=None):
+    def __init__(self, endpoint: str, api_key: Optional[str] = None, secret: Optional[str] = None, key_pool: Optional[KeyPool] = None, cache=None):
         super().__init__()
         self.endpoint = endpoint
-        self.api_key = api_key
-        self.secret = secret
+        if not key_pool:
+            if not api_key or not secret:
+                raise ValueError(
+                    'You must provide either a key_pool or an api_key and a secret combination.'
+                )
+            self.key_pool = None
+            self.api_key = api_key
+            self.secret = secret
+        else:
+            self.key_pool = key_pool
+            self.api_key = self.key_pool.get_key()
+            self.secret = self.key_pool.get_secret()
+
         self.cache = cache
 
         # in case of error, retry at most 3 times, waiting
@@ -96,6 +176,14 @@ class HotelbedsClient(Session):
 
         res = super().request(method, url, **kwargs)
 
+        if res.status_code == 403 and self.key_pool is not None:
+            logger.info('Rate limited for the day. Rotating keys.')
+            if not self.key_pool.next_key():
+                raise KeyPoolOverflow('No more keys in pool')
+            self.api_key = self.key_pool.get_key(),
+            self.secret = self.key_pool.get_secret()
+            self.request(method, url, **kwargs)
+
         if self.cache is not None and res.ok:
             logger.info(f'Adding to cache {url}')
             self.cache[key] = pickle.dumps(res)
@@ -106,9 +194,10 @@ class HotelbedsClient(Session):
 if __name__ == '__main__':
     hotelbeds = HotelbedsClient(
         input('endpoint: '),
-        input('api key: '),
-        input('api secret: '),
+        api_key=input('api key: '),
+        secret=input('api secret: '),
     )
+
     res = hotelbeds.get('/hotel-api/1.0/status')
     print('response:', res)
     print('body:', res.json())
