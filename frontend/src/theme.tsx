894a284e5c05e12a148967c2b7bd664a1088553c
NoahCardoza <noahcardoza@gmail.com>
2023-04-25 12:38:38 -0700

    theme alerts

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,50 @@
+import { alpha, darken } from '@mui/material';
+import { createTheme, responsiveFontSizes, Theme } from '@mui/material/styles';
+
+const alertThemer =
+  (severity: 'success' | 'info' | 'warning' | 'error') =>
+  ({ theme }: { theme: Theme }) => ({
+    backgroundColor: alpha(darken(theme.palette[severity].light, 0.8), 0.8),
+    backdropFilter: 'blur(10px)',
+  });
+
+export default responsiveFontSizes(
+  createTheme({
+    palette: {
+      primary: { main: '#615EFF' },
+      mode: 'dark',
+    },
+    components: {
+      MuiButton: {
+        defaultProps: {
+          variant: 'contained',
+        },
+        styleOverrides: {
+          root: {
+            borderRadius: '6px',
+            textTransform: 'none',
+            lineHeight: '1.5',
+          },
+        },
+      },
+      MuiAlert: {
+        styleOverrides: {
+          standardError: alertThemer('error'),
+          standardInfo: alertThemer('info'),
+          standardSuccess: alertThemer('success'),
+          standardWarning: alertThemer('warning'),
+        },
+      },
+      MuiMenu: {
+        defaultProps: {
+          disableScrollLock: true,
+        },
+        styleOverrides: {
+          paper: {
+            borderRadius: '8px',
+          },
+        },
+      },
+    },
+  }),
+);
