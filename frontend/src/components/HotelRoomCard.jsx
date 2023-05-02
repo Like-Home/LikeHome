3cf0644d92f2c826f9210f0f68aa14e5ecc5e2e7
NoahCardoza <noahcardoza@gmail.com>
2023-05-02 11:30:54 -0700

    APP-157: fix expanded hotel room cards

--- frontend/src/components/HotelRoomCard.jsx
+++ frontend/src/components/HotelRoomCard.jsx
@@ -2,7 +2,17 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-nested-ternary */
 
-import { Stack, ListItemText, Typography, Card, CardContent, CardActions, Button, ListItemIcon } from '@mui/material';
+import {
+  Stack,
+  ListItemText,
+  Typography,
+  Card,
+  CardContent,
+  CardActions,
+  Button,
+  ListItemIcon,
+  CardMedia,
+} from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2/Grid2';
 import Person from '@mui/icons-material/Person';
 import ExpandMore from '@mui/icons-material/ExpandMore';
@@ -31,15 +41,16 @@ export default function HotelRoomCard({
           width: '100%',
           display: 'flex',
           flexDirection: 'column',
-          justifyContent: 'space-between',
         }}
       >
-        <img
-          {...createHotelbedsSrcSetFromPath(room.images[0].path)}
-          alt="Room Preview"
-          loading="lazy"
-          style={{ width: '100%', height: '200px', objectFit: 'cover' }}
-        />
+        <CardMedia>
+          <img
+            {...createHotelbedsSrcSetFromPath(room.images[0].path)}
+            alt="Room Preview"
+            loading="lazy"
+            style={{ width: '100%', height: '200px', objectFit: 'cover' }}
+          />
+        </CardMedia>
         <CardContent sx={{ pt: 0.5, pb: 0 }}>
           <Typography variant="h6" textTransform="capitalize">
             {room.name.toLowerCase()}
@@ -70,7 +81,7 @@ export default function HotelRoomCard({
             ) : null}
           </Stack>
         </CardContent>
-        <CardActions sx={{ pb: 2, px: 2, justifyContent: 'space-between', alignItems: 'end' }}>
+        <CardActions sx={{ pb: 2, px: 2, justifyContent: 'space-between', alignItems: 'end', flexGrow: 1 }}>
           <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
             {formatCurrency(room.rates[0].net)}
           </Typography>
